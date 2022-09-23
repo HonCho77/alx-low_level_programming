1645b2dc412fb985b8acb25cad9c0d2f0c689544
HonCho77 <arthurboatengsamuel@gmail.com>
2022-09-22 17:13:29 -0700

    ALX

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,19 @@
 #include "main.h"
-
 /**
- * print_number - prints # using _putchar function
- * @n: the integer to print
- * Return: void
- */
-
+* print_number - Prints any integer with putchar
+* @n: Number to prints
+* Return: Nothing
+*/
 void print_number(int n)
 {
-	int copy, nth, size = 1, ones = n % 10;
-
-	n /= 10;
-	copy = n;
-	if (ones < 0)
-	{
-		ones *= -1, copy *= -1, n *= -1;
-		_putchar('-');
-	}
-	if (copy > 0)
-	{
-		while (copy / 10 != 0)
-			copy /= 10, size *= 10;
-		while (size > 0)
-		{
-			nth = n / size;
-			_putchar('0' + nth);
-			n -= nth * size;
-			size /= 10;
-		}
-	}
-	_putchar('0' + ones);
+unsigned int x;
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+x = n;
+if (x / 10)
+print_number(x / 10);
+_putchar(x % 10 + '0');
 }
